e420a04a50a49ffefbeefe95112c994eec8bbcd2
Andy Summers <demon314159@gmail.com>
2024-01-15 02:45:14 -0500

    Add minimal quaternion support to rotate ball on any axis

--- examplea/toy.cpp
+++ examplea/toy.cpp
@@ -5,14 +5,17 @@
 #include "toy.h"
 #include "flipper_model.h"
 #include "ball_model.h"
+#include "quaternion.h"
+#include "pi.h"
+#include <math.h>
 
 #define ANIMATION_0_SPEED 30.0
-#define ANIMATION_1_SPEED 90.0
+#define ANIMATION_1_SPEED 180.0
 #define ANIMATION_2_SPEED 15.0
 #define ANIMATION_3_SPEED 30.0
 
 Toy::Toy()
-    : m_model( new BallModel(2.0, PaintCan(1.0, 0.0, 0.0), PaintCan(1.0, 1.0, 1.0), PaintCan(0.0, 0.0, 1.0), 1.0))
+    : m_model( new BallModel(3.0, PaintCan(1.0, 0.0, 0.0), PaintCan(1.0, 1.0, 1.0), PaintCan(0.0, 0.0, 1.0), 1.0))
     , m_animation_0_angle(0.0)
     , m_animation_1_angle(0.0)
     , m_animation_2_angle(0.0)
@@ -47,29 +50,28 @@ void Toy::advance(int nanoseconds)
 
 void Toy::build_model()
 {
-//    CadModel flipper = FlipperModel(1.0, PaintCan(1.0, 1.0, 0.0), PaintCan(1.0, 0.0, 0.0), 1.0, 0.6, 5.0, 2.0, 0.2, 0.5, 0.0, 0.0);
-
-//    m_model->add(flipper, 0.0, 0.0, 0.0);
+    FlipperModel flipper(2.0, PaintCan(1.0, 1.0, 0.0), PaintCan(1.0, 0.0, 0.0), 1.0, 0.4, 5.0, 2.0, 0.2, 1.6);
+    m_model->add(flipper, 0.0, 0.0, 0.0);
     m_model->magnify(0.5);
 }
 
 Matrix4x4 Toy::get_animation_matrix(int i) const
 {
     Matrix4x4 mm;
+    float f = 1.0 / sqrt(2.0);
 
     mm.unity();
     if (i == 0) {
+        mm.translate(-2.0, 0.0, 0.0);
         mm.rotate_ay(m_animation_0_angle);
     } else if (i == 1) {
-        mm.translate(2.0, 0.0, -2.0);
-        mm.rotate_ax(m_animation_1_angle);
+        Quaternion q(m_animation_1_angle * PI / 180.0, {f, 0.0, f});
+        mm = q.to_matrix();
+//        mm.rotate_ax(m_animation_1_angle);
     } else if (i == 2) {
-        mm.translate(0.0, -1.0, 0.0);
         mm.rotate_ay(m_animation_2_angle);
     } else if (i == 3) {
-        mm.translate(0.0, 1.0, 0.0);
         mm.rotate_ay(-m_animation_3_angle);
-        mm.rotate_az(m_animation_3_angle * 3.0);
     }
     return mm;
 }
